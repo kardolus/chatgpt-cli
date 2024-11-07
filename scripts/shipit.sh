9132948bf4c18ab579d0a163b12189c9a6a418ad
kardolus <kardolus@gmail.com>
2024-11-07 08:37:00 +0100

    chore: update release process in shipit.sh

--- scripts/shipit.sh
+++ scripts/shipit.sh
@@ -28,17 +28,17 @@ echo "Updating dependencies..."
 echo "Running all tests..."
 ./scripts/all-tests.sh
 
-# Step 5: Create and push git tag
+# Step 5: Generate release notes by diffing from the latest tag to HEAD
+echo "Generating release notes..."
+git diff "$(git rev-list --tags --max-count=1)"..HEAD  -- . ":(exclude)vendor" | chatgpt -n -p ../prompts/write_release_notes.md for the 'how to update' section explain you can use brew upgrade chatgpt-cli or do a direct download of the binaries for your specific OS. The version we are releasing is "$TAG"
+
+# Step 6: Create and push git tag
 echo "Creating git tag..."
 git tag -a "$TAG" -m "$MESSAGE"
 git push origin --tags
 
-# Step 6: Create binaries
+# Step 7: Create binaries
 echo "Creating binaries..."
 ./scripts/binaries.sh
-
-# Step 7: Generate release notes by diffing from the latest tag to HEAD
-echo "Generating release notes..."
-git diff "$(git rev-list --tags --max-count=1)"..HEAD  -- . ":(exclude)vendor" | chatgpt -n -p ../prompts/write_release_notes.md for the 'how to update' section explain you can use brew upgrade chatgpt-cli or do a direct download of the binaries for your specific OS. The version we are releasing is "$TAG"
 
 echo "Release complete. Tag $TAG has been created, pushed, and binaries are ready."
